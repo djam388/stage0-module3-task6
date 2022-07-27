f4076ebf1833b55208470206b31af39828539a66
djam388 <djam388@hotmail.com>
2022-07-27 15:36:30 +0500

    Attempt #2

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -4,7 +4,7 @@ public class IntegerTypeCasting {
     public static void main(String[] args) {
 
         long first = 1000L;
-        int second = 10000;
+        int second = 1000;
 
         System.out.println(first);
         System.out.println(second);
--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -12,7 +12,7 @@ public class PrePostIncrement {
         x++;
         System.out.println("Expected x is 40, x = " + x);
         // print the updated value of a, b, c, and d  
-        System.out.println("The updated value of a = " + a + ", b = " + ++b + ", c = " + c + " and d = " + ++d);
+        System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + c + " and d = " + d);
 
     }
 }
