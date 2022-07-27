a94b30dc8a7df5d7e39d145d9719e9fd3cd48f7a
djam388 <djam388@hotmail.com>
2022-07-27 15:49:38 +0500

    Attempt #3

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -4,7 +4,7 @@ public class IntegerTypeCasting {
     public static void main(String[] args) {
 
         long first = 1000L;
-        int second = 1000;
+        int second = (int) first;
 
         System.out.println(first);
         System.out.println(second);
